3d23707165abd0b8ad2301560bc7ad409f0020ba
Jonathan Menna <jmenna7@icloud.com>
2021-04-05 02:15:06 -0400

    4/5/21

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 app.all('/', (req, res) => {
-  res.send('Hello World');
+  const randomInt = Math.floor(Math.random() * 2);
+  if (randomInt === 0) {
+    res.send('Hello World');
+  }
+  if (randomInt === 1) {
+    res.throw('oops');
+  }
 });
 
 module.exports = app;

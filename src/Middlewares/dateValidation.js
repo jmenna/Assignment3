e8f68d2dbbe84773e7de1f1633cd9cfa7489179d
Jonathan Menna <jmenna7@icloud.com>
2021-04-05 13:46:58 -0400

    4/5/21

--- src/Middlewares/dateValidation.js
+++ src/Middlewares/dateValidation.js
@@ -5,52 +5,10 @@ const { checkBody, validationResult } = require('express-validator');
 
 const app = express();
 
-/* app.any('/', checkBody('date-validation').exists(), (req, res) => {
-  try {
-    validationResult(req).throw();
-    if (checkBody('date-validation').isNaN()) {
-      res
-        .status(StatusCodes.UNAUTHORIZED)
-        .send(ReasonPhrases.UNAUTHORIZED);
-    } else {
-      const currentDate = Date.now();
-    }
-  } catch (err) {
-    res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send(ReasonPhrases.UNAUTHORIZED);
-  }
-}); */
-
-/* module.exports = app.all('/', (req, res) => {
-  if (req.query.date.exists()) {
-    if (req.query.date.isNaN()) {
-      res
-        .status(StatusCodes.UNAUTHORIZED)
-        .send(ReasonPhrases.UNAUTHORIZED);
-    } else {
-      const dateValidationParam = parseInt(req.query.datevalidation, 10);
-      const currentDate = Date.now();
-
-      if (currentDate - dateValidationParam < 300) {
-        req.dateValidation = currentDate;
-      } else {
-        res
-          .status(StatusCodes.UNAUTHORIZED)
-          .send(ReasonPhrases.UNAUTHORIZED);
-      }
-    }
-  } else {
-    res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send(ReasonPhrases.UNAUTHORIZED);
-  }
-}); */
-
 module.exports = app.all('/', (req, res) => {
   try {
     const dateValidationParam = parseInt(req.query.datevalidation, 10);
-    const currentDate = Date.now();
+    const currentDate = Date.now() / 1000;
 
     if (currentDate - dateValidationParam < 300) {
       req.dateValidation = currentDate;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const deleteMiddleware = require('./Middlewares/deleteMiddleware');
-const loggerMiddleware = require('./Middlewares/logger');
+// const loggerMiddleware = require('./Middlewares/logger');
 // const dateValidation = require('./Middlewares/dateValidation');
 const router = require('./Routers/router');
 // const errorMiddleware = require('./Middlewares/errorHandler');
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(deleteMiddleware);
 // app.use(dateValidation);
-app.use(loggerMiddleware);
+// app.use(loggerMiddleware);
 app.use(router);
 // app.use(errorMiddleware);
 
